c40524d7b1872415838416b7865fdeb2c8ac8477
Jack Moore <jtmthf@mst.edu>
2018-03-10 23:28:42 -0600

    setup typescript webpack

---
+++ config/webpack.config.ts
@@ -0,0 +1,39 @@
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.ts|\.tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+              ['@babel/preset-env', {
+                useBuiltins: 'usage',
+                modules: false,
+                targets: {
+                  browsers: ['> 1%', 'ie 11']
+                }
+              }]
+            ]
+          }
+        },
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+}
+
+export default config;
