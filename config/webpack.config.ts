732f12b828949e7d8ce9550b37f0181d7bb0e71d
Jack Moore <jtmthf@mst.edu>
2018-03-11 04:03:13 -0500

    Added config function

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,41 +1,55 @@
 import * as webpack from 'webpack';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const config: webpack.Configuration = {
+interface Options {
+  prod?: boolean;
+}
+
+interface RuleOptions {
+  target: 'server' | 'client';
+}
+
+const createRules: (env: RuleOptions) => webpack.Rule[] = ({ target }) => [
+  {
+    test: /(\.ts|\.tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+        presets: [
+          '@babel/preset-typescript',
+          '@babel/preset-react',
+          [
+            '@babel/preset-env',
+            {
+              useBuiltins: 'usage',
+              modules: false,
+              targets:
+                target === 'server'
+                  ? { node: 'current' }
+                  : { browsers: ['> 1%', 'ie 11'] },
+            },
+          ],
+        ],
+        plugins: ['react-hot-loader/babel'],
+      },
+    },
+  },
+];
+
+type FunctionConfig = (env: Options) => webpack.Configuration;
+
+const createClientConfig: FunctionConfig = ({ prod = false }) => ({
+  mode: prod ? 'production' : 'development',
   entry: './src/index.tsx',
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
   },
   module: {
-    rules: [
-      {
-        test: /(\.ts|\.tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            presets: [
-              '@babel/preset-typescript',
-              '@babel/preset-react',
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltins: 'usage',
-                  modules: false,
-                  targets: {
-                    browsers: ['> 1%', 'ie 11'],
-                  },
-                },
-              ],
-            ],
-            plugins: ['react-hot-loader/babel'],
-          },
-        },
-      },
-    ],
+    rules: createRules({ target: 'client' }),
   },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
-};
+});
 
-export default config;
+export default [createClientConfig];
